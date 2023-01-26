a623efa39659416f8e4870d808a23c0598e362d3
T-Lind <tiernanxklindauer@gmail.com>
2023-01-25 20:09:45 -0600

    successful fractional integral plot

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 from fractional_calculus.differint import fractional_integral
 
-print(fractional_integral(2.4, 1.2, lambda x: x**2, res=0.001))
+
+
+def f(x):
+    return x ** 2
+
+
+print(fractional_integral(2.9, 1.2, f, res=0.001))
+
+x = np.arange(0, 4, 0.01)
+y = [f(v) for v in x]
+y_p1 = [fractional_integral(1.5, t, f) for t in x]
+y_p2 = [fractional_integral(2.3 + 1, t, f) for t in x]
+plt.plot(x, y, label="f(x)")
+plt.plot(x, y_p1, label="F1.5(x)")
+plt.plot(x, y_p2, label="F2.3(X)")
+plt.legend()
+plt.title("Fractional integration in Python")
+plt.show()
